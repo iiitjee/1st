e8ebce99253db4d9544b4ec233759c68356a720a
Joe McCain III <jo3mccain@icloud.com>
2022-12-28 20:01:49 +0000

    Update

--- bin/pzzldbot/src/services/oai.rs
+++ bin/pzzldbot/src/services/oai.rs
@@ -12,23 +12,13 @@ use openai::{
 use scsys::AsyncResult;
 use serde::{Deserialize, Serialize};
 
-pub async fn chatgpt(prompt: &str) -> AsyncResult {
-    let oai = OpenAI::from_env(Some("OPENAI_SECRET_KEY"));
-    let req = oai.create_request(prompt);
-    let res = oai.response(req).await?;
-    println!("{:?}", res.choices);
-    Ok(())
-}
-
 #[derive(Clone, Debug, Default, Deserialize, Hash, Eq, PartialEq, Serialize)]
-pub struct OpenAI {
-    secret_key: String,
-}
+pub struct OpenAI(String);
 
 impl OpenAI {
     pub fn new(secret_key: Option<String>) -> Self {
         let secret_key = secret_key.unwrap_or_default();
-        Self { secret_key }
+        Self(secret_key)
     }
     pub fn from_env(secret_key: Option<&str>) -> Self {
         let secret_key = match std::env::var(secret_key.unwrap_or("OPENAI_API_KEY")) {
@@ -38,10 +28,10 @@ impl OpenAI {
         Self::new(secret_key)
     }
     pub fn is_auth(&self) -> bool {
-        self.secret_key.len() > 1 && self.secret_key.starts_with("sk-")
+        self.0.len() > 1 && self.0.starts_with("sk-")
     }
     pub fn client(&self) -> openai::Client {
-        openai::Client::new().with_api_key(self.secret_key.as_str())
+        openai::Client::new().with_api_key(self.0.as_str())
     }
     pub fn create_request(&self, prompt: &str) -> CreateCompletionRequest {
         CreateCompletionRequest {
@@ -61,22 +51,18 @@ impl OpenAI {
     }
 }
 
-#[cfg(test)]
-#[cfg(feature = "test_env")]
-mod tests {
-    use super::*;
+pub async fn chatgpt(prompt: &str) -> AsyncResult {
+    let oai = OpenAI::from_env(Some("OPENAI_SECRET_KEY"));
+    let req = oai.create_request(prompt);
+    let res = oai.response(req).await?;
+    println!("{:?}", res.choices);
+    Ok(())
+}
 
-    #[test]
-    fn test_openai_default() {
-        let a = OpenAI::from_env(Some("OPENAI_API_KEY"));
-        assert!(a.is_auth())
-    }
+pub struct ChatGPT(OpenAI);
 
-    #[tokio::test]
-    async fn test_openai_completion() {
-        let oai = OpenAI::from_env(Some("OPENAI_API_KEY"));
-        let req = oai.create_request("What is music theory?");
-        let res = oai.response(req).await;
-        assert!(res.is_ok())
-    }
+impl ChatGPT {
+    // pub async fn response(&self) -> String {
+    //     self.0.create_request(prompt)
+    // }
 }
